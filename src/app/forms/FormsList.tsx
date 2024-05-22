14af862266570594ee78d60879d22d3853cee2d3
Otto <justigo86@gmail.com>
2024-05-22 08:40:29 -0400

    created cards that map through the forms db data on the homepage allowing users to select forms

---
+++ src/app/forms/FormsList.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { forms } from "@/db/schema";
+import { InferSelectModel } from "drizzle-orm";
+import Link from "next/link";
+import React from "react";
+
+type Form = InferSelectModel<typeof forms>;
+type Props = {
+  forms: Form[];
+};
+
+const FormsList = (props: Props) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 m-5 p-4 gap-4">
+      {props.forms.map((form: Form) => (
+        <Card key={form.id} className="max-w-md">
+          <CardHeader>
+            <CardTitle>{form.name}</CardTitle>
+            <CardDescription>{form.description}</CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Link className="w-full" href={`/forms/edit/${form.id}`}>
+              <Button className="w-full">View</Button>
+            </Link>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default FormsList;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Header from "@/components/ui/header";
 import FormGenerator from "./form-generator";
 import { Providers } from "./providers";
+import { db } from "@/db";
+import FormsList from "./forms/FormsList";
 
-export default function Home() {
+export default async function Home() {
+  const forms = await db.query.forms.findMany();
+
   return (
     <Providers>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center p-24">
         <FormGenerator />
+        <FormsList forms={forms} />
       </main>
     </Providers>
   );
