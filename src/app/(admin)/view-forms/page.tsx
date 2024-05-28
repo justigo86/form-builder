151a1be0d0938e5985396f6c49062aed05f6732c
Otto <justigo86@gmail.com>
2024-05-27 23:59:51 -0400

    started making navbar - also added types/nav-types

--- src/app/(admin)/view-forms/page.tsx
+++ src/app/(admin)/view-forms/page.tsx
@@ -1,4 +1,5 @@
 import { getUserForms } from "@/app/actions/getUserForms";
+import FormsList from "@/app/forms/FormsList";
 import { forms as dbForms } from "@/db/schema";
 import { InferSelectModel } from "drizzle-orm";
 import React from "react";
@@ -7,7 +8,12 @@ type Props = {};
 
 const page = async (props: Props) => {
   const forms: InferSelectModel<typeof dbForms>[] = await getUserForms();
-  return <div>my forms</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold px-4 m-5">My Forms</h1>
+      <FormsList forms={forms} />
+    </div>
+  );
 };
 
 export default page;
---
+++ src/components/icons.tsx
@@ -0,0 +1,22 @@
+import {
+  LibraryBig,
+  LineChart,
+  List,
+  LucideIcon,
+  PieChart,
+  Settings,
+  Settings2,
+  UserRoundCog,
+} from "lucide-react";
+
+export type Icoon = LucideIcon;
+
+export const Icons = {
+  library: LibraryBig,
+  lineChart: LineChart,
+  pieChart: PieChart,
+  settings: Settings,
+  settings2: Settings2,
+  userRoundCog: UserRoundCog,
+  list: List,
+};
