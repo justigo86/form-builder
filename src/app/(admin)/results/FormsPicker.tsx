fe26fdcf2e2293f6346442a32c81b63997fe270e
Otto <justigo86@gmail.com>
2024-06-03 07:54:54 -0400

    continued updating FormsPicker to show dropdown for selecting form options

--- src/app/(admin)/results/FormsPicker.tsx
+++ src/app/(admin)/results/FormsPicker.tsx
@@ -1,4 +1,16 @@
-import React from "react";
+"use client";
+
+import React, { useCallback } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectGroup,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 type SelectProps = {
   value: number;
@@ -11,11 +23,48 @@ type Props = {
 
 const FormsPicker = (props: Props) => {
   const { options } = props;
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const formId = searchParams.get("formId") || options[0].value.toString();
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+      return params.toString();
+    },
+    [searchParams]
+  );
+
   return (
-    <div>
-      {options.map((option) => (
-        <p key={option.value}>{option.label}</p>
-      ))}
+    <div className="flex gap-2 items-center">
+      <Label className="font-bold">Select a form</Label>
+      <Select value={formId}>
+        <SelectTrigger className="w-[240px]">
+          <SelectValue placeholder={options[0].label} />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {options.map((option) => (
+              <SelectItem
+                key={option.value}
+                value={option.value.toString()}
+                onChange={() =>
+                  router.push(
+                    pathname +
+                      "?" +
+                      createQueryString("id", option.value.toString())
+                  )
+                }
+              >
+                {option.label}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
     </div>
   );
 };
