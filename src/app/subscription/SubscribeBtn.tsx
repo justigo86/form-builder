abdd009cdfaee0db8794cdbbd777efd1fbab7aab
Otto <justigo86@gmail.com>
2024-06-08 15:12:23 -0400

    updated upgrade your plan button via subscribeBtn using stripe checkout-session api

---
+++ src/app/subscription/SubscribeBtn.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { getStripe } from "@/lib/stripe-client";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+type Props = {
+  userId?: string;
+  price: number;
+};
+
+const SubscribeBtn = ({ userId, price }: Props) => {
+  const router = useRouter();
+
+  const handleCheckout = async (price: number) => {
+    if (!userId) {
+      router.push("/login");
+    }
+
+    try {
+      const { sessionId } = await fetch("api/stripe/checkout-session", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ price }),
+      }).then((res) => res.json());
+
+      console.log("sessionId:", sessionId);
+      const stripe = await getStripe();
+      stripe?.redirectToCheckout({ sessionId });
+    } catch (err) {
+      console.log("Error:", err);
+    }
+  };
+  return (
+    <button className="underline" onClick={() => handleCheckout(price)}>
+      Upgrade your plan
+    </button>
+  );
+};
+
+export default SubscribeBtn;
--- src/components/navigation/upgradeAcctBtn.tsx
+++ src/components/navigation/upgradeAcctBtn.tsx
@@ -1,13 +1,17 @@
 import { getUserForms } from "@/app/actions/getUserForms";
 import { MAX_FREE_FORMS } from "@/lib/utils";
-import Link from "next/link";
 import React from "react";
 import ProgressBar from "../progressBar";
+import SubscribeBtn from "@/app/subscription/SubscribeBtn";
+import { auth } from "@/auth";
+
 //upgrade button and account information for sidebar
 const UpgradeAcctBtn = async () => {
+  const session = await auth();
   const forms = await getUserForms();
   const formCount = forms.length;
   const percent = (formCount / MAX_FREE_FORMS) * 100;
+  const userId = session?.user?.id;
 
   return (
     <div className="p-4 mb-4 text-left text-xs">
@@ -16,10 +20,7 @@ const UpgradeAcctBtn = async () => {
         {formCount} out of {MAX_FREE_FORMS} forms generated
       </p>
       <p>
-        <Link href="/setting" className="underline">
-          Upgrade your plan
-        </Link>{" "}
-        for unlimited forms.
+        <SubscribeBtn userId={userId} price={20} /> for unlimited forms.
       </p>
     </div>
   );
